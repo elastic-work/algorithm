ca9573d844d2d21abcbc9fa13b2edda948b17323
DaMingYue <1149298307@qq.com>
2020-08-02 22:08:30 +0800

    :sparkls:整数反转

---
+++ 数学/整数反转_dmy/Reverse_1.java
@@ -0,0 +1,35 @@
+class Reverse_1{
+
+    public static void main(String[] args) {
+        int res = reverse(120);
+        System.out.println(res);
+    }
+    public int reverse(int i){
+        int res = i;
+        if (i<0){
+            res=0-i;
+        }
+        String str = Integer.toString(res);
+        char []cs = str.toCharArray();
+        char []cs2 = new char[cs.length];
+        for (int j=0;j<cs.length;j++){
+            cs2[j]=cs[cs.length-1-j];
+            if (cs2[j]=='0'){
+                cs2[j]=' ';
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int j=0;j<cs2.length;j++){
+            sb.append(cs2[j]);
+        }
+        try{
+            res=Integer.parseInt(sb.toString().trim());
+        }catch (NumberFormatException e){
+            return 0;
+        }
+        if (i<0){
+            return -res;
+        }
+        return res;
+    }
+}
---
+++ 数学/整数反转_dmy/Reverse_2.java
@@ -0,0 +1,27 @@
+class Reverse_1{
+
+    public static void main(String[] args) {
+        int res = reverse(120);
+        System.out.println(res);
+    }
+    public int reverse(int i){
+        //返回结果初始值
+        int res = 0;
+        //过滤掉最后一位数为0或者当前数为0
+        while (i != 0) {
+            // i%10 = i的个位数
+            int pop = i % 10;
+            //结果大于 int类型最大值/10 或者 （结果等于int类型最大值/10 并且 i的个位数 大于7 判断是否超出int的最大值
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && pop > 7))
+                return 0;
+            //结果小于 int类型最小值/10 或者 （结果等于int类型最小值/10 并且 i的个位数 小于-8 判断是否超出int的最小值
+            if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && pop < -8))
+                return 0;
+            //每次将结果乘以10再加i的个位数
+            res = res * 10 + pop;
+            //每次去除个位数
+            i /= 10;
+        }
+        return res;
+    }
+}
